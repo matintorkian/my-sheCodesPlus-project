736b24532aa19e7b60e67018ba6909a03a0c8df3
Matin Torkian <matin.torkian.92@gmail.com>
2022-12-22 20:30:26 +0330

    added the function to make the api call for forecast

--- src/code.js
+++ src/code.js
@@ -1,3 +1,37 @@
+function displayForecast(response) {
+  console.log(response.data.daily);
+  let forecastElement = document.querySelector("#forecast");
+  let forecastHtml = `<div class ="row">`;
+  let days = ["Tue", "Wed", "Thu", "Fri", "Sat"];
+  days.forEach(function (day) {
+    forecastHtml =
+      forecastHtml +
+      `
+                <div class="col-2">
+                  <div class="weather-forecast-date">${day}</div>
+                  <img
+                    src="http://openweathermap.org/img/wn/50d@2x.png"
+                    alt="#"
+                    width="42"
+                  />
+                  <div class="weather-forecast-temp">
+                    <span class="weather-forecast-temp-min">12°</span>
+                    <span class="weather-forecast-temp-max">18°</span>
+                  </div>
+                </div>
+             `;
+  });
+
+  forecastHtml = `${forecastHtml}` + `</div>`;
+  forecastElement.innerHTML = forecastHtml;
+}
+
+function getForecast(coordinates) {
+  let apiKey = "bf54175800a55e59e6c4d6461deeef12";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayForecast);
+}
+
 function showWeatherData(response) {
   celciusTemperature = response.data.main.temp;
   //get the temperature
@@ -20,6 +54,7 @@ function showWeatherData(response) {
     "src",
     `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
   );
+  getForecast(response.data.coord);
 }
 
 function searchCity(event) {
@@ -48,33 +83,6 @@ function toCelcius(event) {
   mainTemp.innerHTML = Math.round(convertedToFarenheit);
   farenheitTemperatureElement.classList.remove("active");
   celciusTemperatureElement.classList.add("active");
-}
-
-function displayForecast() {
-  let forecastElement = document.querySelector("#forecast");
-  let forecastHtml = `<div class ="row">`;
-  let days = ["Tue", "Wed", "Thu", "Fri", "Sat"];
-  days.forEach(function (day) {
-    forecastHtml =
-      forecastHtml +
-      `
-                <div class="col-2">
-                  <div class="weather-forecast-date">${day}</div>
-                  <img
-                    src="http://openweathermap.org/img/wn/50d@2x.png"
-                    alt="#"
-                    width="42"
-                  />
-                  <div class="weather-forecast-temp">
-                    <span class="weather-forecast-temp-min">12°</span>
-                    <span class="weather-forecast-temp-max">18°</span>
-                  </div>
-                </div>
-             `;
-  });
-
-  forecastHtml = `${forecastHtml}` + `</div>`;
-  forecastElement.innerHTML = forecastHtml;
 }
 
 // Displaying the real time and date on the screen
